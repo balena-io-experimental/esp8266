f6b9294623eaccc19f5b5c249b1bcd3d15fdd488
Joseph Roberts <joe@resin.io>
2017-04-07 08:31:50 +0100

    Implement ID endpoint

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
 #include <Arduino.h>
-/*
-  To upload through terminal you can use: curl -F "image=@firmware.bin" <mac>.local/update
-*/
-
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
 #include <ESP8266HTTPUpdateServer.h>
 
 const char* applicationUUID = "1234567890";
@@ -27,7 +21,6 @@ void setup(void) {
 
     WiFi.setAutoConnect(true);
     WiFi.setAutoReconnect(true);
-    WiFi.hostname(applicationUUID);
 
     Serial.print("Connecting");
     WiFi.begin(ssid, password);
@@ -38,10 +31,12 @@ void setup(void) {
     }
     Serial.println("\nConnected");
 
-    MDNS.begin(applicationUUID);
-    httpUpdater.setup(&httpServer);
+    httpServer.on("/id", [](){
+        httpServer.send(200, "text/plain", applicationUUID);
+    });
+
     httpServer.begin();
-    MDNS.addService("http", "tcp", 90);
+    httpUpdater.setup(&httpServer);
 }
 
 void loop(void) {
