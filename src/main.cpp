24f2f7837b4be334de61f176eabe3da2388908a9
Joe Roberts <joe@resin.io>
2017-08-14 15:14:03 +0100

    Implement avahi/mdns client

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 
 Resin resin;
 String applicationUUID = "1234567890";
+String deviceType = "ESP8266";
+String split = "_";
 String ssid = "resin-hotspot";
 String password = "resin-hotspot";
 
 void setup(void) {
-    resin.Setup(applicationUUID, ssid, password);
+    resin.Setup(deviceType, applicationUUID, split, ssid, password);
 }
 
 void loop(void) {
--- src/resin.cpp
+++ src/resin.cpp
@@ -3,8 +3,10 @@
 Resin::Resin(){}
 Resin::~Resin(){}
 
-void Resin::Setup(String applicationUUID, String ssid, String password, bool led) {
+void Resin::Setup(String  deviceType, String applicationUUID, String split, String ssid, String password, bool led) {
+    _deviceType = deviceType;
     _applicationUUID = applicationUUID;
+    _split = split;
     _ssid = ssid;
     _password = password;
     _led = led;
@@ -14,22 +16,25 @@ void Resin::Setup(String applicationUUID, String ssid, String password, bool led
         digitalWrite(LED_BUILTIN, HIGH);
     }
 
+    String host = _deviceType + _split + _applicationUUID + _split + ESP.getChipId();
+
     WiFi.setAutoConnect(true);
     WiFi.setAutoReconnect(true);
+    WiFi.hostname(host);
     WiFi.begin(_ssid.c_str(), _password.c_str());
     while (WiFi.status() != WL_CONNECTED) {
-        delay(100);
+        delay(250);
+    }
+
+    while (!MDNS.begin(host.c_str())) {
+        delay(250);
     }
 
     _httpServer = ESP8266WebServer(80);
-    _httpUpdater = ESP8266HTTPUpdateServer();
-
-    _httpServer.on("/id", [&](){
-        _httpServer.send(200, "text/plain", _applicationUUID.c_str());
-    });
-
-    _httpServer.begin();
     _httpUpdater.setup(&_httpServer);
+    _httpServer.begin();
+
+    MDNS.addService("http", "tcp", 80);
 }
 
 void Resin::Loop() {
